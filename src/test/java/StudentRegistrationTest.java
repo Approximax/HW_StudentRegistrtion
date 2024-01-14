07660d885a8a787298c24e12d2c6dce6e2ce3100
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-01-14 23:00:45 +0300

    Added tests. Check up`s incomming

--- src/test/java/StudentRegistrationTest.java
+++ src/test/java/StudentRegistrationTest.java
@@ -2,6 +2,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class StudentRegistrationTest {
 
@@ -10,7 +13,25 @@ public class StudentRegistrationTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
+    }
+
+    @Test
+    void studentRegistrationTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("David");
+        $("#lastName").setValue("Doe");
+        $("#userEmail").setValue("asd@qwe.ty");
+//        $("#gender-ratio-3").click();
+        $x("//label[]").selectRadio("Other");
+        $("#userNumber").setValue("+12345678");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__day--025").click();
+        $("#hobbies-checkbox-2").click();
+        $("#upoadPicture").uploadFromClasspath("testPicture.png");
+        $("#currentAddress").setValue("st.Test");
+        $("#state").click();
+
+
     }
 }
