10974479f6c7e233e0d7f52b74813a1c15bfdf5d
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-02-15 18:23:06 +0300

    refactored all student registration tests to lambda steps added remote conf for selenoid added util for attachments and imported it into tests

--- src/test/java/demoqa/tests/StudentRegistrationTest.java
+++ src/test/java/demoqa/tests/StudentRegistrationTest.java
@@ -1,18 +1,24 @@
 package demoqa.tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import demoqa.pages.RegistrationPage;
 import demoqa.pages.components.ResultTableComponent;
+import demoqa.utils.Attach;
 import demoqa.utils.RandomUtils;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 
 
-public class StudentRegistrationTest  extends TestBase {
+public class StudentRegistrationTest {
 
     RegistrationPage registrationPage = new RegistrationPage();
     ResultTableComponent resultTableComponent = new ResultTableComponent();
@@ -35,14 +41,43 @@ public class StudentRegistrationTest  extends TestBase {
             year = randomUtils.getRandomYear(),
             subject = randomUtils.getRandomSubject();
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+
     @Test
     @Tag("minimumTest")
+    @DisplayName("Минимально допустимый набор данных для регистрации нового студента")
+    @Severity(SeverityLevel.BLOCKER)
     void minimumDataRegistrationTest() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
 
         step("Открываем сайт c проверяемой формой", () -> registrationPage.openPage());
 
-        step("Устанавливаем тестовые данные студента", () -> registrationPage.setFirstName(firstName)
+        step("Устанавливаем минимально необходимые для регистрации тестовые данные студента", () -> registrationPage.setFirstName(firstName)
                 .setLastName(lastName)
                 .setGender(gender)
                 .setUserNumber(phoneNumber)
@@ -56,23 +91,31 @@ public class StudentRegistrationTest  extends TestBase {
 
     @Test
     @Tag("negativeTest")
+    @DisplayName("Отсутствие регистрации нового студента при неправильном заполнении формы")
+    @Severity(SeverityLevel.BLOCKER)
     void negativeRegistrationTest() {
-        registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName("")
-                .setGender(gender)
-                .setUserNumber(phoneNumber)
-                .submit();
 
-        resultTableComponent.checkAbsence();
+        step("Открываем сайт c проверяемой формой", () -> registrationPage.openPage());
+
+        step("Некорректно заполняем форму регистрации тестовыми данными", () ->registrationPage.setFirstName(firstName)
+                        .setLastName("")
+                        .setGender(gender)
+                        .setUserNumber(phoneNumber)
+                        .submit());
+
+        step("Проверяем, что таблица с данными студента не появилась, после некорректной регистрации",
+                () -> resultTableComponent.checkAbsence());
     }
 
     @Test
     @Tag("fullTest")
+    @DisplayName("Успешная регистрация с полным набором данных нового студента")
+    @Severity(SeverityLevel.BLOCKER)
     void fullDataRegistrationTest() {
 
-        registrationPage.openPage()
-                .setFirstName(firstName)
+        step("Открываем сайт c проверяемой формой", () -> registrationPage.openPage());
+
+        step("Заполнение формы регистрации полным набором данных", () -> registrationPage.setFirstName(firstName)
                 .setLastName(lastName)
                 .setUserEmail(email)
                 .setGender(gender)
@@ -84,9 +127,9 @@ public class StudentRegistrationTest  extends TestBase {
                 .setAddress(address)
                 .setState(state)
                 .setCity(city)
-                .submit();
+                .submit());
 
-        resultTableComponent.checkAppearance()
+        step("Проверяем корректность заполненных данных студента после регистрации в форме", () -> resultTableComponent.checkAppearance()
                 .checkTableValue("Student Name", firstName + " " + lastName)
                 .checkTableValue("Student Email", email)
                 .checkTableValue("Gender", gender)
@@ -96,7 +139,6 @@ public class StudentRegistrationTest  extends TestBase {
                 .checkTableValue("Hobbies", hobby)
                 .checkTableValue("Picture", "testPicture.png")
                 .checkTableValue("Address", address)
-                .checkTableValue("State and City", state + " " + city);
-
+                .checkTableValue("State and City", state + " " + city));
     }
 }
